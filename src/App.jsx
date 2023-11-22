03e6ee1c9e955a695e1db488ca466e7525bcc99e
Luc-Nick <lucero.nico216@gmail.com>
2023-11-22 20:36:27 -0300

    slider funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,14 @@ import './App.css'
 
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css/sea-green';
+import Slider from './components/Slider';
 
 function App() {
   /* const [count, setCount] = useState(0) */
 
   return (
     <>
-      <Splide aria-label="My Favorite Images"
-          options={ {
-            rewind: true,
-            width : 800,
-            gap   : '1rem',
-          } }
-        >
-        <SplideSlide>
-          <img className="splide__slide" src="/calden.jpg" alt="Image 1" />
-        </SplideSlide>
-        <SplideSlide>
-          <img className="splide__slide" src="/festival.jpg" alt="Image 2" />
-        </SplideSlide>
-        <SplideSlide>
-          <img className="splide__slide" src="/scooter.jpg" alt="Image 3" />
-        </SplideSlide>
-      </Splide>
+      <Slider />
     </>
   )
 }
